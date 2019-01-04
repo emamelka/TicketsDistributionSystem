4c810b73ea51f7b9101057f079f5c9029233b443
emamelka <edyta.mamelka1@gmail.com>
2019-01-04 20:47:11 +0100

    Update project

---
+++ src/main/java/pl/emamelka/ticketsDistributionSystem/TicketsDistributionSystemApplication.java
@@ -0,0 +1,14 @@
+package pl.emamelka.ticketsDistributionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsDistributionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsDistributionSystemApplication.class, args);
+	}
+
+}
+
